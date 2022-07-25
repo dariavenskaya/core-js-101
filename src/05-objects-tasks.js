879b2c206e901b92cee031cbbef74b3c00fa3b54
dariavenskaya <venskayadaria@yandex.by>
2022-07-25 12:39:30 +0300

    feat: add fromJSON

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -55,10 +55,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  // const proto = JSON.parse(json)
-  // return proto;
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  const values = Object.values(obj);
+  return new proto.constructor(...values);
 }
 
 
