6ebcbaa279d71b4481a9a0a4ac3a3dc5016d7faa
dariavenskaya <venskayadaria@yandex.by>
2022-07-25 11:52:50 +0300

    feat: add toNumber

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -182,7 +182,6 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
-  // throw new Error('Not implemented');
   return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
@@ -233,10 +232,8 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  // const res = Number(value);
-  // return typeof res === 'number' ? res : def;
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number(value) ? Number(value) : def;
 }
 
 module.exports = {
